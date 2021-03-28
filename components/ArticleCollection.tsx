22c929e19e8271839329606d8a1fd365780517b5
Kazuki Matsuzaki <kazuki0924@gmail.com>
2021-03-29 08:29:48 +0900

    add: types

--- components/ArticleCollection.tsx
+++ components/ArticleCollection.tsx
@@ -1,9 +1,40 @@
-const ArticleCollection = <T extends {}>({ collection }: { collection: T }) => {
+import { toJSON } from '../utils/toJSON';
+
+export interface Article {
+  title: string;
+  tag_list: string[];
+}
+
+export const fetchArticles = () =>
+  fetch('https://dev.to/api/articles').then(toJSON);
+
+const ArticleCollection = ({
+  collection: articles,
+}: {
+  collection: Article[] | undefined;
+}) => {
   return (
-    <>
-      <div>Article 1 </div>
-      <div>Article 2 </div>
-    </>
+    <div className='bg-white shadow rounded'>
+      <ul className='divide-y divide-gray-200'>
+        {articles?.map(({ title, tag_list }: Article) => (
+          <li>
+            <a href='' className='block hover:bg-gray-100'></a>
+            <div className='p-4'>
+              <div className='flex justify-between items-center'>
+                <div className='text-lg text-blue-400'>{title}</div>
+                <div className='ml-2'>
+                  {tag_list.map((tag: any) => (
+                    <div className='ml-1 inline-flex px-2 text-xs rounded-full bg-gray-200 text-gray-700'>
+                      {tag}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useQuery } from 'react-query';
-import ArticleCollection from '../components/ArticleCollection';
+import ArticleCollection, {
+  Article,
+  fetchArticles,
+} from '../components/ArticleCollection';
 
 const Home = () => {
-  const toJSON = (res: Response) => res.json();
-  const fetchArticles = () => fetch('https://dev.to/api/articles').then(toJSON);
-
-  const { data: articles, isLoading, error } = useQuery(
+  const { data: articles, isLoading, error } = useQuery<Article[], Error>(
     'articles',
     fetchArticles
   );
@@ -15,7 +15,7 @@ const Home = () => {
   if (error) return (error as { message: string })?.message;
 
   return (
-    <div className='max-w-md mx-auto mt-8'>
+    <div className='max-w-4xl mx-auto mt-4'>
       <ArticleCollection collection={articles} />
     </div>
   );
